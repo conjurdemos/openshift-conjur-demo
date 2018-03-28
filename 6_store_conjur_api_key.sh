bc829895f201d90e0a49071973d87c1fe26ffa7b
Geri Jennings <geri.jennings@cyberark.com>
2018-03-27 20:52:02 -0400

    update script 6 to fix test app name

--- 6_store_conjur_api_key.sh
+++ 6_store_conjur_api_key.sh
@@ -8,7 +8,7 @@ announce "Storing Conjur API key for test app configuration."
 set_project $CONJUR_PROJECT_NAME
 
 # Rotate the test app's Conjur API key to get a new one.
-host_id=conjur/authn-k8s/$AUTHENTICATOR_SERVICE_ID/apps/test-app/*/*
+host_id=conjur/authn-k8s/$AUTHENTICATOR_SERVICE_ID/apps/$TEST_APP_PROJECT_NAME/*/*
 api_key=$(oc exec $(get_master_pod_name) -- conjur host rotate_api_key -h $host_id)
 
 set_project $TEST_APP_PROJECT_NAME
